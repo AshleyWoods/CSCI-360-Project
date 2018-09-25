29b7ffa7b821a06a1ed4eff2304833352064495c
Ashley Woods <woodsae@g.cofc.edu>
2018-09-24 20:12:49 -0400

    Confirmation Pop up window
    The Submit button now displays a pop up window asking for confirmation on the submit. There is also a base for creating a confirmation of saving the form.

--- src/sample/VoterRegistrationController.java
+++ src/sample/VoterRegistrationController.java
@@ -1,6 +1,12 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class VoterRegistrationController {
 
@@ -13,6 +19,7 @@ public class VoterRegistrationController {
     @FXML
     public void handleSubmit(){
         //ASK IF THEY MEAN TO SUBMIT
+        confirmSubmitPopUp();
        //CHECK VALIDITY OF INPUT IN ALL FIELDS
         //CHECK THE PERSON TO SEE IF THEY'RE ALLOWED TO VOTE?--HOW?
         //SAVE DATA
@@ -20,5 +27,45 @@ public class VoterRegistrationController {
 
 
         main.showLogin();
+    }
+
+    public void confirmSubmitPopUp() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("ConfirmFormSubmission.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+
+            Stage confirmSubmit = new Stage();
+            confirmSubmit.setTitle("Confirm Form Submission");
+            Scene confirmSubmitScene = new Scene(page);
+            confirmSubmit.setScene(confirmSubmitScene);
+
+            confirmSubmit.showAndWait();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void confirmSavePopUp() {
+
+        //None of these files currently exist, don't uncomment the lines until they do
+        /* try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("ConfirmFormSave.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+
+            Stage confirmSave = new Stage();
+            confirmSave.setTitle("Form Saved");
+            Scene confirmSaveScene = new Scene(page);
+            confirmSave.setScene(confirmSaveScene);
+
+            confirmSave.showAndWait();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
     }
 }
